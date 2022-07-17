d47dfd9008061ae4c9d9eab6f44d7032f1c8bd17
w6fux5 <w6fux5@gmail.com>
2022-07-17 20:28:56 +0800

    create background and background move

---
+++ 01/src/scripts/Background.js
@@ -0,0 +1,44 @@
+import * as PIXI from "pixi.js";
+import { Globals } from "./Globals";
+
+export class Background {
+  constructor() {
+    this.speed = 10;
+    this.container = new PIXI.Container();
+    this.createSprites();
+  }
+
+  createSprites() {
+    this.sprites = [];
+
+    for (let i = 0; i < 3; i++) {
+      this.createSprite(i);
+    }
+  }
+
+  createSprite(i) {
+    const sprite = new PIXI.Sprite(Globals.resources["background"].texture);
+    sprite.x = sprite.width * i;
+    sprite.y = 0;
+    this.container.addChild(sprite);
+    this.sprites.push(sprite);
+  }
+
+  move(sprite, offset) {
+    const spriteRightX = sprite.x + sprite.width;
+    const screenLeftX = 0;
+
+    if (spriteRightX <= screenLeftX) {
+      sprite.x += sprite.width * this.sprites.length;
+    }
+
+    sprite.x -= offset;
+  }
+
+  update(dt) {
+    const offset = this.speed * dt;
+    this.sprites.forEach((sprite) => {
+      this.move(sprite, offset);
+    });
+  }
+}
